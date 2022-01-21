37f365cac690a716c30070f2ff4b206dec145902
fimarluka2505 <241225@edu.p.lodz.pl>
2022-01-21 14:45:36 +0100

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 
 using namespace std;
-int Policz (int a, int b)
-{
-    return a*b;
-}
+
 int main() {
 
     cout<<"Hello World"<<endl;
-    cout<<"W pierwszym branchu 2*3="<<Policz(2,3)<<endl;
     return 0;
 }
